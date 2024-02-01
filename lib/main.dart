60415cbc3f0a31ae8ca9e4b099acbef04bb73c05
ba5liel <baslielselamu2018@gmail.com>
2024-02-01 11:57:43 +0300

    added sentry

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,33 @@ import 'package:get/get.dart';
 import 'package:panda_technician/routes/route.dart';
 import 'package:panda_technician/store/AllProviders.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'app/service/init_app_service.dart';
 
+final transaction = Sentry.startTransaction('init service', 'task');
 Future<void> main() async {
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://6b115b14911040855bdb75a6c02a1ebf@o4506670618378240.ingest.sentry.io/4506670621130752';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: startApp,
+  );
+}
+
+Future<void> startApp() async {
+  try {
+    var x = 1 / 0;
+  } catch (exception, stackTrace) {
+    await Sentry.captureException(
+      exception,
+      stackTrace: stackTrace,
+    );
+  }
   await dotenv.load(fileName: ".env");
 
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,8 +46,8 @@ Future<void> main() async {
 
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
   runApp(const MyApp());
+  transaction.finish();
 }
 
 class MyApp extends StatefulWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,6 +17,7 @@ import flutter_local_notifications
 import geolocator_apple
 import package_info_plus
 import path_provider_foundation
+import sentry_flutter
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
@@ -34,6 +35,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
